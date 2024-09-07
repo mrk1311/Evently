f6ec36859b221763e03edc9528d1eda42e91ed02
mrk1311 <mpruszkowski11@gmail.com>
2024-09-07 16:48:00 +0200

    Tried to swithc to react-leaflet

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,40 +1,83 @@
 import React, { useState, useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import "./Map.css";
 
-const MapComponent = ({ position }) => {
-    const mapRef = useRef(null);
-    const markerRef = useRef(null);
-    const circleRef = useRef(null);
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
 
-    useEffect(() => {
-        if (!mapRef.current) {
-            const mapInstance = L.map("map").setView([51.505, -0.09], 13);
-            L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-                maxZoom: 19,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            }).addTo(mapInstance);
-            mapRef.current = mapInstance;
-        }
-    }, []);
+const MapComponent = () => {
+    const center = [48.8566, 2.3522];
 
-    useEffect(() => {
-        if (position && mapRef.current) {
-            const { lat, lng, accuracy } = position;
+    // hard coded markers
+    const markers = [
+        {
+            geocode: [48.86, 2.3522],
+            popUp: "Hello, I am pop up 1",
+        },
+        {
+            geocode: [48.85, 2.3522],
+            popUp: "Hello, I am pop up 2",
+        },
+        {
+            geocode: [48.855, 2.34],
+            popUp: "Hello, I am pop up 3",
+        },
+    ];
 
-            if (markerRef.current) {
-                mapRef.current.removeLayer(markerRef.current);
-                mapRef.current.removeLayer(circleRef.current);
-            }
+    // // custom Icon
+    //   const customIcon = new Icon({
+    //     iconUrl: ""
+    //     iconSize: [25, 41] // 38, 38 ?
+    //   });
 
-            markerRef.current = L.marker([lat, lng]).addTo(mapRef.current);
-            circleRef.current = L.circle([lat, lng], { radius: accuracy }).addTo(mapRef.current);
+    function LocationMarker() {
+        const [position, setPosition] = useState(null);
+        const map = useMap();
 
-            mapRef.current.setView([lat, lng]);
-        }
-    }, [position]);
+        useEffect(() => {
+            map.locate().on("locationfound", function (e) {
+                setPosition(e.latlng);
+                map.flyTo(e.latlng, map.getZoom());
+                const radius = e.accuracy;
+                const circle = L.circle(e.latlng, radius);
+                circle.addTo(map);
+            });
+        }, [map]);
 
-    return <div id="map" style={{ height: "100%", width: "100%" }}></div>;
+        return position === null ? null : (
+            <Marker position={position}>
+                <Popup>You are here</Popup>
+            </Marker>
+        );
+    }
+
+    return (
+        <MapContainer center={center} zoom={13}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            />
+
+            <LocationMarker />
+            <MarkerClusterGroup
+                chunkedLoading
+                // singleMarkerMode
+            >
+                {markers.map((marker, index) => (
+                    <Marker
+                        key={index}
+                        position={marker.geocode} /* icon={customIcon} */
+                    >
+                        <Popup>
+                            <h2> Event </h2>
+                            {marker.popUp}
+                        </Popup>
+                    </Marker>
+                ))}
+            </MarkerClusterGroup>
+        </MapContainer>
+    );
 };
 
 export default MapComponent;
