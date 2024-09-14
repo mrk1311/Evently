38f9c360eaf725095c3fd835b515e018a5cffabc
mrk1311 <mpruszkowski11@gmail.com>
2024-09-14 18:40:02 +0200

    added react-leaflet to vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react()],
     build: {
         rollupOptions: {
-            external: ["react-leaflet"],
+            external: ["react-leaflet", "react-leaflet-cluster"],
         },
     },
 });
