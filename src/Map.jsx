755d566109ef3e7aad01e405fb2ce9c3279069a0
mrk1311 <mpruszkowski11@gmail.com>
2024-09-15 15:35:01 +0200

    bug fix

--- src/Map.jsx
+++ src/Map.jsx
@@ -58,7 +58,7 @@ const MapComponent = ({ position, userPosition }) => {
     ];
 
     const userIcon = new Icon({
-        iconUrl: "dist/assets/map-pin.png",
+        iconUrl: "src/assets/map-pin.png",
         iconSize: [35, 35], // size of the icon
         // iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
         // popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
